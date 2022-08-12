35182702a6c219b782cdbab196addef80e363a6d
Lukas Forst <lukas@forst.dev>
2022-08-12 17:14:33 +0200

    update ktor to latest version

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val detekt = "1.20.0"
-    const val ktor = "2.0.3"
-    const val jupiterVersion = "5.8.2"
+    const val ktor = "2.1.0"
+    const val jupiterVersion = "5.9.0"
 }
 
 object Libs {
@@ -15,11 +15,11 @@ object Libs {
 
 
     object Test {
-        const val logBack = "ch.qos.logback:logback-classic:1.3.0-alpha5"
+        const val logBack = "ch.qos.logback:logback-classic:1.3.0-beta0"
 
         const val ktorServerTestHost = "io.ktor:ktor-server-test-host:${Versions.ktor}"
 
-        const val mockk = "io.mockk:mockk:1.11.0"
+        const val mockk = "io.mockk:mockk:1.12.5"
 
         const val jupiterApi = "org.junit.jupiter:junit-jupiter-api:${Versions.jupiterVersion}"
         const val jupiterParams = "org.junit.jupiter:junit-jupiter-params:${Versions.jupiterVersion}"
