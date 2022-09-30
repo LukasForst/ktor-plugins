4bf4a79fd60ee809f9ac86c9f306058457efda54
Lukas Forst <lukas@forst.dev>
2022-09-30 15:11:34 +0200

    update deps

--- ktor-content-security-policy/src/test/kotlin/dev/forst/ktor/csp/TestMinimalExampleApp.kt
+++ ktor-content-security-policy/src/test/kotlin/dev/forst/ktor/csp/TestMinimalExampleApp.kt
@@ -1,6 +1,7 @@
 package dev.forst.ktor.csp
 
 import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
 import io.ktor.server.application.Application
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Test
@@ -9,15 +10,20 @@ import kotlin.test.assertNull
 
 class TestMinimalExampleApp {
     @Test
+    fun `test csp header is correct`() {
+        assertEquals("Content-Security-Policy", HttpHeaders.ContentSecurityPolicy)
+    }
+
+    @Test
     fun `test minimal example app works as expected`() = testApplication {
         application(Application::minimalExample)
         // this should return csp header
         var responseWithCsp = client.get("/")
-        assertEquals("default-src 'self'", responseWithCsp.headers["Content-Security-Policy"])
+        assertEquals("default-src 'self'", responseWithCsp.headers[HttpHeaders.ContentSecurityPolicy])
         responseWithCsp = client.get("/specific")
-        assertEquals("default-src 'none'", responseWithCsp.headers["Content-Security-Policy"])
+        assertEquals("default-src 'none'", responseWithCsp.headers[HttpHeaders.ContentSecurityPolicy])
         // this should not
         val skippedResponse = client.get("/ignored")
-        assertNull(skippedResponse.headers["Content-Security-Policy"])
+        assertNull(skippedResponse.headers[HttpHeaders.ContentSecurityPolicy])
     }
 }
